8e23bec3a3dcb4ce81df9bdfb15ab374b9b1db7b
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2022-07-31 14:32:17 +0400

    dashboard-area-fixed

--- src/feature/Dashboard/Area.js
+++ src/feature/Dashboard/Area.js
@@ -3,54 +3,154 @@ import { useTranslation } from "react-i18next";
 
 function Area() {
   const { t } = useTranslation();
-  const title = t("dashboard.area-title");
-  const subtitle = t("dashboard.area-subtitle");
   const series = [
     {
-      name: "STOCK ABC",
-      data: [12, 34, 59],
+      name: "February",
+      data: [
+        {
+          x: 2019,
+          y: 20,
+        },
+        {
+          x: 2020,
+          y: 40,
+        },
+        {
+          x: 2021,
+          y: 60,
+        },
+        {
+          x: 2022,
+          y: 80,
+        },
+      ],
+    },
+    {
+      name: "March",
+      data: [
+        {
+          x: 2019,
+          y: 50,
+        },
+        {
+          x: 2020,
+          y: 100,
+        },
+        {
+          x: 2021,
+          y: 150,
+        },
+        {
+          x: 2022,
+          y: 200,
+        },
+      ],
+    },
+    {
+      name: "April",
+      data: [
+        {
+          x: 2019,
+          y: 100,
+        },
+        {
+          x: 2020,
+          y: 150,
+        },
+        {
+          x: 2021,
+          y: 200,
+        },
+        {
+          x: 2022,
+          y: 250,
+        },
+      ],
     },
   ];
   const options = {
+    tooltip: {
+      enabled: false,
+    },
     chart: {
       type: "area",
       height: 350,
-      zoom: {
-        enabled: false,
-      },
     },
     dataLabels: {
-      enabled: true,
+      enabled: false,
     },
     stroke: {
       curve: "straight",
     },
 
     title: {
-      text: title,
+      text: t("dashboard.area-title"),
       align: "left",
       style: {
-        color: "#C7C7C7",
         fontSize: "20px",
+        color: "#C7C7C7",
       },
     },
     subtitle: {
-      text: subtitle,
+      text: t("dashboard.area-subtitle"),
       align: "left",
       style: {
         color: "#8E8E93",
         fontSize: "16px",
       },
     },
-    labels: [120, 204, 30],
     xaxis: {
-      type: "datetime",
+      type: "year",
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
     },
     yaxis: {
-      opposite: true,
+      tickAmount: 3,
+      floating: false,
+      style: {
+        fontSize: "16px",
+      },
+      title: {
+        text: "Total",
+        rotate: 0,
+        offsetX: 90,
+        offsetY: -120,
+        style: {
+          fontSize: "16px",
+          color: "#4F4F4F",
+        },
+      },
+
+      labels: {
+        style: {
+          colors: "#8e8da4",
+        },
+        offsetY: -7,
+        offsetX: 0,
+      },
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
     },
-    legend: {
-      horizontalAlign: "left",
+    fill: {
+      opacity: 0.5,
+    },
+    grid: {
+      yaxis: {
+        lines: {
+          offsetX: -30,
+        },
+      },
+      padding: {
+        left: 20,
+      },
     },
   };
 
@@ -60,8 +160,8 @@ function Area() {
         options={options}
         series={series}
         type="area"
-        height={400}
-        width='100%'
+        height={298}
+        width={439.48}
       />
     </div>
   );
